cffee92f501af08dad0efda82e4d777f0ba194c6
Eber <eberjoe@gmail.com>
2020-03-07 14:49:44 -0300

    Add 02

---
+++ 02/02.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *v, *r, produto = 1, n, i, j;
+    printf("\nEntre o tamanho do vetor 'v': ");
+    scanf("%d", &n);
+    v = malloc(sizeof(int) * n);
+    r = malloc(sizeof(int) * n);
+    for (i = 0; i < n; i++) {
+        printf("Entre um valor para a posicao %d: ", i);
+        scanf("%d", v + i);
+    }
+    printf("\n\nSeu vetor: [ ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", v[i]);
+    }
+    printf("]\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            produto *= (i != j)? v[j] : 1;
+        r[i] = produto;
+        produto = 1;
+    }
+    printf("\n\nVetor resultante: [ ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", r[i]);
+    }
+    printf("]\n");
+    return 0;
+}
